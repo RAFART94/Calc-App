f88cae79de189cee4c7dd09160f641b7ff990184
RAFART94 <danielgrs94@gmail.com>
2024-03-31 16:12:09 +0100

    Estructura de Aplicación Calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,38 @@ const App = () => {
     console.log('Renderización de App')//para que aparezaca en Inspeccionar>Consola
     return (
     <main className="react-calculator">
-        Calc App
+        <div className="result">
+        </div>
+        <div className="numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+        </div>
+        <div className="functions">
+            <button>
+                clear
+            </button>
+            <button>
+                r
+            </button>
+        </div>
+        <div className="math-operations">
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>/</button>
+            <button>=</button>
+        </div>
+
+
+
     </main>)
 }
 
